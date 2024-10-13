cbf6688695a4e7af6cd60966dfa7210e3d15deaa
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-10-14 01:06:55 +0530

    Add files via upload

---
+++ 03_DOM/03_multipleElement/script.js
@@ -0,0 +1,19 @@
+/*
+// Selecting multiple elements at a same time
+var h=document.querySelectorAll("h1")
+console.log(h);
+    
+h.forEach(function(e){
+    console.log(e)
+})
+*/
+
+//document.getElementById("box")   
+// document.getElementsByClassName("box")
+
+// var h=document.querySelector("h1")
+// h.textContent="bye"
+
+var box = document.querySelector("#box")
+box.innerHTML = "<h1>Virat</h1>"
+// box.textContent="<h1>Virat</h1>"
