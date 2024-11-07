e5d2b44ad6276a7d1c72508904da3c4af67b1c61
AK47 <147317580+Anish-Kanrar@users.noreply.github.com>
2024-11-07 22:49:52 +0530

    Update app.js

--- 05_MEN/part-1/app.js
+++ 05_MEN/part-1/app.js
@@ -1,3 +1,4 @@
+/*
 // const catMe = require('cat-me')
 
 // console.log(catMe());
@@ -18,3 +19,72 @@ const server = http.createServer((req, res) => {
 })
 
 server.listen(3000)
+*/
+const express = require('express')
+const morgan = require('morgan')
+const app = express();
+const dbConnection = require('./config/db')
+const userModel = require('./models/user')
+
+app.use(morgan('dev'))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
+app.set("view engine", 'ejs')
+
+app.get('/',
+    (req, res, next) => {
+        const a = 5;
+        const b = 10;
+        console.log(a + b)
+
+        next()
+
+    }, (req, res) => {
+        res.render('index')
+    })
+
+app.get('/about', (req, res) => {
+    res.send('About Page')
+})
+
+app.get('/profile', (req, res) => {
+    res.send('Profile Page')
+})
+
+app.get('/register', (req, res) => {
+    res.render('register')
+})
+
+app.post('/register', async (req, res) => {
+    const { username, email, password } = req.body //destructuring
+
+    const newUser = await userModel.create({
+        username: username,
+        email: email,
+        password: password
+    })
+
+    res.send(newUser)
+})
+
+app.get('/get-users', (req, res) => {
+
+    userModel.findOne({
+        username: 'k'
+    }).then((user) => {
+
+        console.log(user)
+
+        res.send(user)
+    })
+})
+
+app.post('/get-form-data', (req, res) => {
+    console.log(req.body)
+    res.send('data received')
+})
+
+app.listen(3000)
